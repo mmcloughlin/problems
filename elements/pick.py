6d17e73b6895a33f3f0f07def0f352dc6b282d09
Michael McLoughlin <mmcloughlin@gmail.com>
2016-05-15 16:50:30 -0700

    new question picker

---
+++ elements/pick.py
@@ -0,0 +1,29 @@
+import random
+import sys
+import os.path
+
+QUESTIONS_DIR = './questions'
+
+
+def read_questions_file(name):
+    questions_file = os.path.join(QUESTIONS_DIR, name)
+    with open(questions_file) as f:
+        return [q.strip() for q in f]
+
+
+# arguments
+sources = sys.argv[1:] if len(sys.argv) > 1 else ['all']
+sources.append('-done')
+
+# form set of possible questions
+questions = set()
+for source in sources:
+    if source[0] == '+':
+        questions |= set(read_questions_file(source[1:]))
+    elif source[0] == '-':
+        questions -= set(read_questions_file(source[1:]))
+    else:
+        questions |= set(read_questions_file(source))
+
+# pick random one
+print random.choice(list(questions))
