9653e11369588e6f6a89085158caa613025a8d22
Michael McLoughlin <mmcloughlin@gmail.com>
2019-02-17 23:47:49 -0800

    leetcode: invert-binary-tree

---
+++ leetcode/invert-binary-tree/invert.go
@@ -0,0 +1,18 @@
+package invert
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	return &TreeNode{
+		Val:   root.Val,
+		Left:  invertTree(root.Right),
+		Right: invertTree(root.Left),
+	}
+}
