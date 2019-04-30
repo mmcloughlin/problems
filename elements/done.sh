ae4c93c44e66a550ad4d9b0bca31544325735453
Michael McLoughlin <mmcloughlin@gmail.com>
2019-04-29 19:31:08 -0700

    meta: update helper scripts for elements

--- elements/done.sh
+++ elements/done.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 problem=$1
+lang=${2:-go}
 
-dir=${problem/./\/}
+dir=${lang}/${problem/./\/}
 
-echo $problem $(date +"%Y-%m-%dT%H:%M:%S%z") >> "questions/done"
+echo $problem $(date +"%Y-%m-%dT%H:%M:%S%z") >> "questions/${lang}"
 git add $dir "questions/done"
 git commit -m "solution to $problem"
--- elements/pick.py
+++ elements/pick.py
@@ -1,19 +1,22 @@
+#!/usr/bin/env python3
+
 import random
 import sys
 import os.path
 
 QUESTIONS_DIR = './questions'
+LANGUAGE = 'go'
 
 
 def read_questions_file(name):
     questions_file = os.path.join(QUESTIONS_DIR, name)
     with open(questions_file) as f:
-        return filter(None, [q.strip() for q in f])
+        return filter(None, [q.split()[0] for q in f])
 
 
 # arguments
 sources = sys.argv[1:] if len(sys.argv) > 1 else ['all']
-sources.append('-done')
+sources.append('-' + LANGUAGE)
 
 # form set of possible questions
 questions = set()
@@ -26,4 +29,4 @@ for source in sources:
         questions |= set(read_questions_file(source))
 
 # pick random one
-print random.choice(list(questions))
+print(random.choice(list(questions)))
