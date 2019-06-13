6095842ffe007a12ec8c2093850515aa4e046616
Michael McLoughlin <mmcloughlin@gmail.com>
2019-06-12 21:04:09 -0400

    faster version of subarray sums equal to k

--- leetcode/subarray-sum-equals-k/solution.go
+++ leetcode/subarray-sum-equals-k/solution.go
@@ -1,28 +1,13 @@
 package solution
 
 func subarraySum(nums []int, k int) int {
-	type sum struct{ val, count int }
-	sums := []sum{}
 	total := 0
-
+	prefixcount := map[int]int{0: 1}
+	prefix := 0
 	for _, num := range nums {
-		seen := false
-		for i := range sums {
-			sums[i].val += num
-			if sums[i].val == num {
-				sums[i].count++
-				seen = true
-			}
-		}
-		if !seen {
-			sums = append(sums, sum{val: num, count: 1})
-		}
-		for i := range sums {
-			if sums[i].val == k {
-				total += sums[i].count
-			}
-		}
+		prefix += num
+		total += prefixcount[prefix-k]
+		prefixcount[prefix]++
 	}
-
 	return total
 }
